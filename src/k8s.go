c2e3df4708e99300061f17ce0900a4836ed9458c
Markus Blaschke <mblaschke82@gmail.com>
2018-01-30 22:23:48 +0100

    Add k8s automatic secret creation

--- src/k8s.go
+++ src/k8s.go
@@ -44,33 +44,40 @@ func (k *Kubernetes) Client() (clientset *kubernetes.Clientset) {
 
 
 func (k *Kubernetes) ApplySecret(namespace, secretName, filename string, content []byte) error {
-
-	var secret v1.Secret
+	var secret *v1.Secret
 	secretExists := false
 
 	option := v12.GetOptions{}
-	if val, err := k.Client().CoreV1().Secrets(namespace).Get(secretName, option); err != nil {
+	if val, err := k.Client().CoreV1().Secrets(namespace).Get(secretName, option); err == nil {
 		if val != nil {
+			// existing
 			secret = val
 			secretExists = true
 		}
-	} else {
-		return err
+	}
+
+	if secret == nil {
+		// create
+		secret = &v1.Secret{}
 	}
 
 	secret.APIVersion = "v1"
 	secret.Namespace = namespace
 	secret.Name = secretName
 	secret.Type = "Opaque"
-	secret.Data = map[string][]byte{}
+
+	if secret.Data == nil {
+		secret.Data = map[string][]byte{}
+	}
+
 	secret.Data[filename] = content
 
 
 	var err error
 	if secretExists {
-		_, err = k.Client().CoreV1().Secrets(namespace).Update(&secret)
+		_, err = k.Client().CoreV1().Secrets(namespace).Update(secret)
 	} else {
-		_, err = k.Client().CoreV1().Secrets(namespace).Create(&secret)
+		_, err = k.Client().CoreV1().Secrets(namespace).Create(secret)
 	}
 
 	return err
